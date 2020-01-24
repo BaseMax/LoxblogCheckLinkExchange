d1a30ee2a044f304f0ea7d732214c063660f180c
Max Base <Max@Asrez.com>
2020-01-25 00:51:23 +0330

    Upload input file and create check.php file

---
+++ check.php
@@ -0,0 +1,13 @@
+<?php
+include "phpnet.php";
+$input=file_get_contents("subdomains.txt");
+$subdomains=explode("\n", $input);
+$subdomains=array_filter($subdomains);
+print_r($subdomains);
+$output=[];
+foreach($subdomains as $subdomain) {
+
+}
+foreach($output as $item=>$bool) {
+	print $item."\n";
+}
