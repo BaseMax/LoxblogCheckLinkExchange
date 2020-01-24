84874db5d00298740f30f482d22003379613a8b3
Max Base <Max@Asrez.com>
2020-01-25 00:56:55 +0330

    Complete check.php code and program

--- check.php
+++ check.php
@@ -6,7 +6,11 @@ $subdomains=array_filter($subdomains);
 print_r($subdomains);
 $output=[];
 foreach($subdomains as $subdomain) {
-
+	$response=get("http://".$subdomain);
+	preg_match('/(\'|\")(captcha_code_filec\.php)(\'|\")/i', $response[0], $has);
+	if(isset($has[2]) && $has[2] == "captcha_code_filec.php") {
+		$output[$subdomain]=true;
+	}
 }
 foreach($output as $item=>$bool) {
 	print $item."\n";
